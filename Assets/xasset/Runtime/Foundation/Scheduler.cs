55d9dd529979edad1071aaabd9e485c2db7e699a
jiyuanfeng <xasset@qq.com>
2023-05-18 18:57:42 +0800

    # 支持动态修改 MaxRequests

--- Assets/xasset/Runtime/Foundation/Scheduler.cs
+++ Assets/xasset/Runtime/Foundation/Scheduler.cs
@@ -5,19 +5,28 @@ namespace xasset
 {
     /// <summary>
     ///     支持自动切片的调度器，可以对单帧密集的更新操作进行负载均衡处理。
-    /// </summary>
+    /// </summary> 
     public class Scheduler : MonoBehaviour
     {
         private static readonly Dictionary<string, RequestQueue> _Queues = new Dictionary<string, RequestQueue>();
         private static readonly List<RequestQueue> Queues = new List<RequestQueue>();
         private static readonly Queue<RequestQueue> Append = new Queue<RequestQueue>();
-        private static float _realtimeSinceStartup;
+        private static float _realtimeSinceStartup;  
         public static bool AutoSlicing { get; set; } = true;
         public static bool Working => Queues.Exists(o => o.working);
-        public static bool Busy => AutoSlicing && Time.realtimeSinceStartup - _realtimeSinceStartup > AutoSliceTimestep;
-        public static float AutoSliceTimestep { get; set; }
+
+        public static bool Busy =>
+            AutoSlicing && Time.realtimeSinceStartup - _realtimeSinceStartup > AutoSliceTimestep;
+
+        public static float AutoSliceTimestep { get; set; } = 1f / 60f;
         public static byte MaxRequests { get; set; } = 10;
 
+        private void Start()
+        { 
+            DontDestroyOnLoad(gameObject);
+        }
+
+        private static byte updateMaxRequests { get; set; } = 10;
 
         private void Update()
         {
@@ -31,6 +40,16 @@ namespace xasset
             foreach (var queue in Queues)
                 if (!queue.Update())
                     break;
+
+            if (updateMaxRequests != MaxRequests)
+            {
+                foreach (var queue in Queues)
+                {
+                    queue.maxRequests = MaxRequests;
+                }
+
+                updateMaxRequests = MaxRequests;
+            }
         }
 
         public static void Enqueue(Request request)
